2c089c417f31d6de64675452cf9251d6d7369aa5
BuildTools <unconfigured@null.spigotmc.org>
2025-07-11 15:48:45 +0200

    WIP praise section

--- src/page-mappers/home/home.mapper.ts
+++ src/page-mappers/home/home.mapper.ts
@@ -1,14 +1,15 @@
 import Hero from "../../components/home/Hero.astro";
+import Praise from "../../components/home/Praise.astro";
 import { HomeSections } from "./home.enum";
 
 //@ts-ignore
 export const homeSections: Record<HomeSections, any> = {
   [HomeSections.HERO_SECTION]: Hero,
-  //   [HomeSections.ABOUT_SECTION]: null,
+  [HomeSections.PRAISE_SECTION]: Praise,
+  // [HomeSections.ABOUT_SECTION]: null,
   //   [HomeSections.CONTACT_SECTION]: null,
   //   [HomeSections.OFFERS_SECTION]: null,
   //   [HomeSections.PROJECTS_SECTION]: null,
-  //   [HomeSections.PRAISE_SECTION]: null,
 };
 
 export function mapHomeSectionToComponent(homeSection: HomeSections) {
