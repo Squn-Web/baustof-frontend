567ca5c368e16394b64f690381cce657ddff71d0
BuildTools <unconfigured@null.spigotmc.org>
2025-08-22 18:41:46 +0200

    feature/singe-project-page, implemented static routing

---
+++ src/api/fetchProjectBySlug/query.ts
@@ -0,0 +1,24 @@
+import { defineQuery } from "groq";
+import type { GetProjectBySlugQueryResult } from "../../../sanity.types";
+import { fetchSanity } from "../../lib/api";
+
+const getProjectBySlugQuery = defineQuery(`
+*[_type == "project" && slug.current == $slug]
+`);
+
+export async function fetchProjectBySlug(
+  slug: string,
+): Promise<GetProjectBySlugQueryResult[number]> {
+  const result = await fetchSanity<GetProjectBySlugQueryResult>(
+    getProjectBySlugQuery,
+    { slug },
+  );
+
+  const project = result[0];
+
+  if (!project) {
+    throw new Error(`No project with slug ${slug} found`);
+  }
+
+  return project;
+}
